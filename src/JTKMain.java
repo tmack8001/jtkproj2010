6a504b17926cbbb270bfb96f3c48c8612aefbc88
blitzkev <blitzkev@190cffa1-cb15-b021-6fb2-1550b25e209d>
2010-05-01 23:32:34 +0000

    support for connecting player/stage in main

--- src/JTKMain.java
+++ src/JTKMain.java
@@ -1,6 +1,8 @@
-import java.util.Random;
-
-
+import javaclient2.PlayerClient;
+import javaclient2.Position2DInterface;
+import javaclient2.SonarInterface;
+import javaclient2.structures.PlayerConstants;
+import javaclient2.structures.sonar.PlayerSonarData;
 
 public class JTKMain {
 
@@ -8,13 +10,23 @@ public class JTKMain {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		int j;
-		Random flushot = new Random();
-		flushot.setSeed(System.currentTimeMillis());
-		for(int i = 0; i < 50; i++){
-			j = flushot.nextInt() % 100;
-			System.out.println(Math.abs(j));
+		int port = 6665;
+		String server = "localhost";
+		
+		if (args.length == 2) {
+			server = args[0];
+			port = Integer.parseInt(args[1]);
 		}
+		
+		PlayerClient robot = new PlayerClient(server, port);
+		SonarInterface sonar = robot.requestInterfaceSonar(0, 
+				PlayerConstants.PLAYER_OPEN_MODE);
+		Position2DInterface motor = robot.requestInterfacePosition2D(0, 
+				PlayerConstants.PLAYER_OPEN_MODE);
+		
+		// turn stuff on.  this might not be necessary
+		sonar.setSonarPower(1);
+		motor.setMotorPower(1);
 	}
 
 }
