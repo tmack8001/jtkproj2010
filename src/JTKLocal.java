2cac9671c2c4822754ba8e22c40441da0088f8ec
james.loomis <james.loomis@190cffa1-cb15-b021-6fb2-1550b25e209d>
2010-05-11 19:41:41 +0000

    half-implemented and untested localization

--- src/JTKLocal.java
+++ src/JTKLocal.java
@@ -1,4 +1,135 @@
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Random;
+
 public class JTKLocal {
+
+	JTKMap map;
+	MotionModel motion;
+	SonarModel sonar;
+	private int N;
+
+	Random rand;
+
+	Sample[] S;
+
+	// sonar info yanked from pioneer.inc
+	private double sonar_x[] =
+		{0.075,0.115,0.150,0.170,0.170,0.150,0.115,0.075};
+	private double sonar_y[] =
+		{0.130,0.115,0.080,0.025,-0.025,-0.080,-0.115,-0.130};
+	private double sonar_h[] = 
+		{1.571,0.873,0.523,0.175,-.175,-.523,-.873,-1.571};
+	
+
+	public JTKLocal(MotionModel motion,SonarModel sonar,int N,JTKMap map) {
+		this.map = map;
+		this.motion = motion;
+		this.sonar = sonar;
+		this.N = N;
+		rand = new Random();
+		S = new Sample[N];
+
+		for(int i=0;i<N;i++)
+			S[i] = new Sample(); //random
+		
+	}
+
+	// function implemented out of:
+	// Artificial Intelligence, 3rd Ed, 
+	// by Russel & Norvig, page 982.
+	public Sample[] update(
+			double speed,
+			double turnrate,
+			double[] sp) {
+
+		Sample T[] = new Sample[N];
+		double W[] = new double[N];
+
+		double totalW = 0.;
+		for(int i=0;i<N;i++) {
+			T[i] = motion.sample(S[i],speed,turnrate);
+			W[i] = T[i].probability(sp);
+			totalW += W[i];
+		}
+
+		double CDF[] = new double[N];
+		double pastCDF = 0.;
+		for(int i=0;i<N;i++) {
+			CDF[i] = W[i] / totalW + pastCDF;
+			pastCDF += CDF[i];
+		}
+
+		System.out.println("integral of CDF: " + pastCDF);
+		
+		// sampling
+		for(int i=0;i<N;i++) {
+			// random U(0,1)
+			double p = rand.nextDouble();
+
+			// binary search through CDF
+			int index = binarysearch(0,N,W,p);
+
+			// pick number at location <-- that's the sampled value
+			S[i] = T[index];
+		}
+
+		return S;
+	}
+
+	public int binarysearch(int start,int end,double array[],double target) {
+		int index = start + (start-end)/2; 
+		if(end <= start)
+			return start;
+		else if(array[index] > target)
+			return binarysearch(start, index-1, array, target);
+		else if(array[index] < target)
+			return binarysearch(index+1, end, array, target);
+		else
+			return index; //unlikely!
+	}
+			
+	public class Sample {
+
+		double X;
+		double Y;
+		double H;
+
+		public Sample() {
+			X = (rand.nextDouble() - .5) * 131.2;
+			Y = (rand.nextDouble() - .5) * 41;
+			H = (rand.nextDouble() - .5) * 2. * Math.PI;
+		}
+
+		// sonar reading probability
+		// (adapted from mapping code.)
+		public double probability(double sp[]) {
+			double prob = 0.;
+
+			for(int i=0;i<8;i++) { // for each sonar
+			  double x = X + sonar_x[i];
+			  double y = Y + sonar_y[i];
+			  double h = H + sonar_h[i];
+			  for(double d=0;d<5.0;d+=0.2) {
+			    for(double theta=-Math.PI/24.;
+			               theta<Math.PI/24.;
+			               theta+=Math.PI/(48.)) {
+			      double obs_prob = sonar.PM_d_theta(sp[i],theta,d);
+			      double x_map = Math.cos(h+theta)*d+X;
+			      double y_map = Math.sin(h+theta)*d+Y;
+			      if(map.workspace(x_map,y_map)) {
+			        prob *= prob; //obstacle
+			      } else {
+			        prob *= (1-prob); //no obstacle
+			      }
+			    }
+			  }
+			}
+
+			return prob;
+		}
+
+	}
 
 }
