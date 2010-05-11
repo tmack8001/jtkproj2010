d265ef9cd2ff34f7a740c432334fbb23d19b01dc
james.loomis <james.loomis@190cffa1-cb15-b021-6fb2-1550b25e209d>
2010-05-11 16:09:16 +0000

    fixed bug in mapping code

--- trunk/src/JTKMap.java
+++ trunk/src/JTKMap.java
@@ -33,7 +33,6 @@ public class JTKMap {
 			DataInputStream in = new DataInputStream(
 				new FileInputStream(f));
 			for(int i=0;i<1600*500;i++) {
-				//workspace = (in.readByte < 0);
 				workspace[i] = in.readByte();
 				cspace[i] = false;
 			}
@@ -86,7 +85,7 @@ public class JTKMap {
 	public static Point2D point2pixels(Point2D p) {
 		Point2D q = new Point2D.Double(
 			 Math.floor(1600.*p.getX() / 131.2 + (1600./2.)),
-		         Math.floor(500.*p.getY()/ 41. + (500./2.)));
+		         500 - Math.floor(500.*p.getY()/ 41. + (500./2.)));
 		System.err.println(q);
 		return q;
 	}
