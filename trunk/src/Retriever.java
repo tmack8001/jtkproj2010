99d306544de2616dd6994f9b08b346a16743210d
drummer8001 <drummer8001@190cffa1-cb15-b021-6fb2-1550b25e209d>
2010-05-14 14:03:41 +0000

    added everything over into Retriever.java (the required file)

--- trunk/src/Retriever.java
+++ trunk/src/Retriever.java
@@ -2,6 +2,9 @@
  * filename: Retriever.java
  */
 
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.geom.Point2D;
 import java.io.BufferedReader;
 import java.io.File;
@@ -9,6 +12,13 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JMenuBar;
+import javax.swing.JPanel;
 
 
 import javaclient2.PlayerClient;
@@ -26,17 +36,29 @@ import javaclient2.structures.sonar.PlayerSonarData;
  *
  */
 public class Retriever {
-
-	//points the robot needs to get to
-	static ArrayList<Point2D> points = new ArrayList<Point2D>(1);
 	
+	//... Components
+	private static JFrame f;
+	
+    private static JMenuBar menu;
+    //private static GridMap gridMap;
+    
+    private static JFrame sonarFrame;
+    private static SonarView sonarView; //this shows the current sonar reading (from James)
+    //private static JTKMapImage particleImage; //this shows the current state of the particles for localization
+    
+    private static JButton sonarButton;
+    private static JButton exitButton;
 	
 	/**
-	 * @param args
+	 * @param 	args[0] host
+	 * 			args[1] port
+	 * 			args[2] filename // point file for path planning
 	 */
 	public static void main(String[] args) {
 		int port = 6665;
 		String server = "localhost";
+		List<Point2D> points = new LinkedList<Point2D>();
 		
 		if (args.length == 2) {
 			server = args[0];
@@ -67,10 +89,63 @@ public class Retriever {
 			}	
 		}
 		
-		//test print of arraylist points
-		for(int x = 0; x < points.size(); x++){
-			System.out.println(points.get(x).toString());
-		}
+		//create GUI
+		f = new JFrame("JTKProj2010 - Retriever");
+		JTKMap map = new JTKMap();
+		JTKMapImage jtk = new JTKMapImage(map);
+		
+		//... Initialize menu
+    	menu = new JMenuBar();
+    	sonarButton = new JButton("Sonar Array");
+    	exitButton = new JButton("Exit Program");
+    
+    	sonarFrame = new JFrame();
+    	sonarView = new SonarView();
+    	sonarFrame.setSize(200, 400);
+    	sonarFrame.add(sonarView);
+    	sonarFrame.setVisible(false);
+    	
+    	menu.add(sonarButton);
+    	menu.add(exitButton);
+    	
+    	sonarButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				sonarFrame.setVisible(true);
+			}
+		});
+        
+        exitButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				f.setVisible(false);
+				f.dispose();	
+				System.exit(0);
+			}
+		});
+    	
+		//add Components
+        JPanel content = new JPanel();
+        content.setLayout(new BorderLayout());
+        content.add(menu, BorderLayout.NORTH);
+        content.add(jtk, BorderLayout.CENTER);
+		
+        f.add(content);
+        
+		f.pack();
+		f.setVisible(true);
+		
+		f.setLocationRelativeTo(null);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		//add waypoints to the map
+		jtk.setPoints(points);
+		
+		SonarModel sonarModel = new SonarModel(10);
+		JTKLocal local = new JTKLocal(sonarModel, 10000, map);
+		
+		//add particles to the map
+		jtk.setParticles(local.S);
 		
 		PlayerClient robot = new PlayerClient(server, port);
 		SonarInterface sonar = robot.requestInterfaceSonar(0, 
@@ -78,49 +153,54 @@ public class Retriever {
 		Position2DInterface motor = robot.requestInterfacePosition2D(0, 
 				PlayerConstants.PLAYER_OPEN_MODE);
 		
-		JTKView view = new JTKView(50, 50, 0.0625);
-		
 		// turn stuff on.  this might not be necessary
 		sonar.setSonarPower(1);
 		motor.setMotorPower(1);
 		
-		while(true) {
+		double sp[] = new double[8];
+		while (true) {
 			float turnRate, speed;
 			
 			// read all the data
 			robot.readAll();
-
-			// don't do anything unless there's data
 			
+			// don't do anything unless there's data
 			if (sonar.isDataReady()) {
-				view.setModel(sonar, motor);
-				view.setVisible(true);
 				
+				//gather data
 				PlayerSonarData sonarData = sonar.getData();
 				float[] ranges = sonarData.getRanges();
-				view.repaint();
+				
+				//update map
+				if(sonarFrame.isVisible()) {
+					sonarView.updateSonars(ranges);
+					sonarFrame.repaint();
+				}
+				jtk.repaint();
 				
 				if (ranges.length == 0)
 					continue;
-                double x = motor.getX();
-                double y = motor.getY();
-                double theta = motor.getYaw();
+
+				for(int i=0;i<8;i++) 
+					sp[i] = ranges[i];
                 
-				if (ranges[0] + ranges[1] < ranges[6] + ranges[7])
-					turnRate = -20.0f * (float)Math.PI / 180.0f;
-				else
-					turnRate = 20.0f * (float)Math.PI / 180.0f;
-				
-				if (ranges[3] < 0.5f)
-					speed = 0.0f;
-				else
-					speed = 0.1f;
+				//execute a random walk
+				double left = sp[0] + sp[1] + sp[2] + sp[3];
+				double right = sp[4] + sp[5] + sp[6] + sp[7];
+				if(sp[3] + sp[4] > 2f) {
+					turnRate = (float)(Math.sqrt(left) - Math.sqrt(right));
+					speed = .5f;
+				} else {
+					speed = 0f;
+					turnRate = (float)Math.PI/12f;
+				}
 				
 				// send the command
 				motor.setSpeed(speed, turnRate);
+				//update particles with given directions
+				local.update((double)speed,(double)turnRate,sp);
 			}
-		}
-				
+		}					
 	}
-
+	
 }
